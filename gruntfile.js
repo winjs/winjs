a2ba4f5cde2e1170fcb7317e01fc8a7439eb9a1c
David Evans <daevans@microsoft.com>
2014-03-19 13:11:10 -0700

    Add comments based on feedback, and fix a bug for running all tests that I noticed.

--- gruntfile.js
+++ gruntfile.js
@@ -372,8 +372,13 @@ module.exports = function (grunt) {
                     for (var i = 0; i < arguments.length; ++i)
                         args.push(arguments[i]);
 
-                    var testArgs = "*.js";
+                    // Default args
+                    if (args.length === 0 || args[0] === "")
+                        args[0] = "*.js";
                     var host = "wwa";
+
+                    // Determine if last argument is a host parameter (not a glob pattern)
+                    // Host parameter is only valid with 1 or more parameters
                     if (args.length > 1) {
                         var last = args[args.length - 1].toLowerCase();
                         if (last.indexOf("*") < 0 && last.indexOf(".") < 0) {
@@ -382,6 +387,7 @@ module.exports = function (grunt) {
                         }
                     }
 
+                    // Build up command string
                     var command = "%_NTTREE%/Corsica/other.2.1.debug/Tools/WebUnit/WebUnit.exe";
                     for (var i = 0; i < args.length; ++i)
                         command +=  " /s:%_NTTREE%/Corsica/other." + version + ".debug/Tests/UnitTests/" + args[i];
