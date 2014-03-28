f1df477e3e0d7adf3293dc5e4502fa58334346b8
seanxu <seanxu@microsoft.com>
2014-03-27 17:43:43 -0700

    Updated unittests.jsproj Changed to always build QUnit test pages

--- tasks/test.js
+++ tasks/test.js
@@ -1,73 +1,68 @@
 module.exports = function (grunt) {
     var config = require("../config.js");
     
-    grunt.registerTask("test", function () {
-        if (!config.inRazzle) {
-            grunt.task.run(["default"]);
-            generateQUnitTestPages();
-        } else {        
-            var args = [];
-            for (var i = 0; i < arguments.length; ++i)
-                args.push(arguments[i]);
+    grunt.registerTask("test", function () {        
+        var args = [];
+        for (var i = 0; i < arguments.length; ++i)
+            args.push(arguments[i]);
 
-            grunt.task.run(["default", "shell:runTests:" + args.join(":")]);
-        }
+        grunt.task.run(["default", "shell:runTests:" + args.join(":")]);
     });
     
-    function generateQUnitTestPages() {
-        var fs = require("fs");
-        
-        function clean(path) {
-            var files = [];
-            if( fs.existsSync(path) ) {
-                files = fs.readdirSync(path);
-                files.forEach(function(file,index){
-                    var curPath = path + "/" + file;
-                    if(fs.lstatSync(curPath).isDirectory()) {
-                        clean(curPath);
-                    } else {
-                        fs.unlinkSync(curPath);
-                    }
-                });
-                fs.rmdirSync(path);
-            }
-        }
-        
-        function extractDependencies(fileContents) {
-            var deps = [];
-        
-            var lines = fileContents.split("\r\n");
-            for (var i = 0; i < lines.length; i++) {
-                var line = lines[i];
-                var processedOne = false;
-                if (line.indexOf("<reference") < 0 || line.indexOf("ms-appx") >= 0) {
-                    if (processedOne) {
-                        break;
-                    } else {
-                        continue;
-                    }
-                }
-                
-                var startIndex = line.indexOf('path="') + 6;
-                var endIndex = line.indexOf('"', startIndex);
-                
-                deps.push(line.substring(startIndex, endIndex));
-                
-                processedOne = true;
-            }
-            return deps;
-        }
-        
-        function arrayIndexOf(arr, obj) {
-            for (var i = 0; i < arr.length; i++) {
-                if (arr[i] === obj) {
-                    return i;
-                }
-            }
-            return -1;
-        }
+    // Generate QUnit test pages
+    var fs = require("fs");
     
-        var testMenuTemplate = 
+    function clean(path) {
+        var files = [];
+        if( fs.existsSync(path) ) {
+            files = fs.readdirSync(path);
+            files.forEach(function(file,index){
+                var curPath = path + "/" + file;
+                if(fs.lstatSync(curPath).isDirectory()) {
+                    clean(curPath);
+                } else {
+                    fs.unlinkSync(curPath);
+                }
+            });
+            fs.rmdirSync(path);
+        }
+    }
+    
+    function extractDependencies(fileContents) {
+        var deps = [];
+    
+        var lines = fileContents.split("\r\n");
+        for (var i = 0; i < lines.length; i++) {
+            var line = lines[i];
+            var processedOne = false;
+            if (line.indexOf("<reference") < 0 || line.indexOf("ms-appx") >= 0) {
+                if (processedOne) {
+                    break;
+                } else {
+                    continue;
+                }
+            }
+            
+            var startIndex = line.indexOf('path="') + 6;
+            var endIndex = line.indexOf('"', startIndex);
+            
+            deps.push(line.substring(startIndex, endIndex));
+            
+            processedOne = true;
+        }
+        return deps;
+    }
+    
+    function arrayIndexOf(arr, obj) {
+        for (var i = 0; i < arr.length; i++) {
+            if (arr[i] === obj) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    var testMenuTemplate = 
 '<!DOCTYPE html>                                                                                                            \r\n\
 <html>                                                                                                                      \r\n\
 <head>                                                                                                                      \r\n\
@@ -87,7 +82,7 @@ module.exports = function (grunt) {
 </body>                                                                                                                     \r\n\
 </html>';
     
-        var testPageTemplate =
+    var testPageTemplate =
 '<!-- saved from url=(0014)about:internet -->\r\n\
 <!DOCTYPE html>                                                                                                             \r\n\
 <html>                                                                                                                      \r\n\
@@ -115,84 +110,83 @@ module.exports = function (grunt) {
 </body>                                                                                                                     \r\n\
 </html>'.replace(/@@TARGETFRAMEWORK/g, config.targetFramework);
     
-        clean("./bin/");
-        fs.mkdirSync("./bin/");
-        fs.mkdirSync("./bin/tests/");
+    clean("./bin/");
+    fs.mkdirSync("./bin/");
+    fs.mkdirSync("./bin/tests/");
+    
+    var dirs = fs.readdirSync("./tests");
+    var tests = "";
+    dirs.forEach(function (dir) {
+        if (!fs.lstatSync("./tests/" + dir).isDirectory() || dir === "TestData" || dir === "TestLib") {
+            return;
+        }
         
-        var dirs = fs.readdirSync("./tests");
-        var tests = "";
-        dirs.forEach(function (dir) {
-            if (!fs.lstatSync("./tests/" + dir).isDirectory() || dir === "TestData" || dir === "TestLib") {
-                return;
+        var html = testPageTemplate;
+        html = html.replace("@@TITLE", dir);
+        
+        var testReferences = "";
+        
+        var srcs = [];
+        var csss = [];
+        var files = fs.readdirSync("./tests/" + dir);
+        for (var i = files.length - 1; i >= 0; i --) {
+            // Some folders have .html test assets, we can ignore those
+            var file = files[i];
+            if (file.indexOf(".css") >= 0) {
+                csss.push(file);
+            } else if (file.indexOf(".js") >= 0) {
+                srcs.push(file);
             }
-            
-            var html = testPageTemplate;
-            html = html.replace("@@TITLE", dir);
-            
-            var testReferences = "";
-            
-            var srcs = [];
-            var csss = [];
-            var files = fs.readdirSync("./tests/" + dir);
-            for (var i = files.length - 1; i >= 0; i --) {
-                // Some folders have .html test assets, we can ignore those
-                var file = files[i];
-                if (file.indexOf(".css") >= 0) {
-                    csss.push(file);
-                } else if (file.indexOf(".js") >= 0) {
-                    srcs.push(file);
-                }
-            }
-            var done = false;
-            while (!done) {
-                done = true;
-                var srcsCopy = srcs.slice(0);
-                for (var i = 0; i < srcs.length; i++) {
-                    var fc = fs.readFileSync("./tests/" + dir + "/" + srcs[i], "utf-8");
-                    var deps = extractDependencies(fc);
-                    var length = srcsCopy.length;
-                    deps.forEach(function (dep) {
-                        if (dep === srcs[i]) {
-                            // Some files reference themselves, this check breaks an infinite loop
-                            return;
+        }
+        var done = false;
+        while (!done) {
+            done = true;
+            var srcsCopy = srcs.slice(0);
+            for (var i = 0; i < srcs.length; i++) {
+                var fc = fs.readFileSync("./tests/" + dir + "/" + srcs[i], "utf-8");
+                var deps = extractDependencies(fc);
+                var length = srcsCopy.length;
+                deps.forEach(function (dep) {
+                    if (dep === srcs[i]) {
+                        // Some files reference themselves, this check breaks an infinite loop
+                        return;
+                    }
+                    if (dep.indexOf(".css") >= 0) {
+                        if (arrayIndexOf(csss, dep) < 0) { 
+                            csss.push(dep);
                         }
-                        if (dep.indexOf(".css") >= 0) {
-                            if (arrayIndexOf(csss, dep) < 0) { 
-                                csss.push(dep);
-                            }
+                    } else {
+                        var index = arrayIndexOf(srcsCopy, dep);
+                        if (index >= 0) {
+                            // If this dependency already exists, we need to reorder it to the bottom
+                            // of the array so it gets loaded first. This usually happens when a dependency
+                            // is in the same folder as the test folder but appears alphabetically before it.
+                            srcsCopy.splice(index, 1);
+                            length--;
                         } else {
-                            var index = arrayIndexOf(srcsCopy, dep);
-                            if (index >= 0) {
-                                // If this dependency already exists, we need to reorder it to the bottom
-                                // of the array so it gets loaded first. This usually happens when a dependency
-                                // is in the same folder as the test folder but appears alphabetically before it.
-                                srcsCopy.splice(index, 1);
-                                length--;
-                            } else {
-                                done = false;
-                            }
-                            // Since the <script> tags get emitted in reverse order, we need to splice in the
-                            // dependencies in reverse-order as well.
-                            srcsCopy.splice(length, 0, dep);
+                            done = false;
                         }
-                    });
-                }
-                srcs = srcsCopy;
+                        // Since the <script> tags get emitted in reverse order, we need to splice in the
+                        // dependencies in reverse-order as well.
+                        srcsCopy.splice(length, 0, dep);
+                    }
+                });
             }
-            
-            for (var i = 0; i< csss.length; i++) {
-                testReferences += '    <link type="text/css" rel="stylesheet" href="' + csss[i] + '" />';
-            }
-            for (var i = srcs.length - 1; i >= 0; i--) {
-                testReferences += '    <script src="' + srcs[i] + '"></script>\r\n';
-            }
-            testReferences = testReferences.substr(0, testReferences.length - 2);
-            html = html.replace("@@TESTREFERENCES", testReferences);
-            fs.mkdirSync("./bin/tests/" + dir);
-            fs.writeFileSync("./bin/tests/" + dir + "/test.html", html);
-            tests += '    <h2><a href="' + dir + '/test.html">' + dir + " tests</a></h2>\r\n";
-        });
-        tests = tests.substr(0, tests.length - 2);
-        fs.writeFileSync("./bin/tests/tests.html", testMenuTemplate.replace("@@TESTS", tests));
-    }
+            srcs = srcsCopy;
+        }
+        
+        for (var i = 0; i< csss.length; i++) {
+            testReferences += '    <link type="text/css" rel="stylesheet" href="' + csss[i] + '" />';
+        }
+        for (var i = srcs.length - 1; i >= 0; i--) {
+            testReferences += '    <script src="' + srcs[i] + '"></script>\r\n';
+        }
+        testReferences = testReferences.substr(0, testReferences.length - 2);
+        html = html.replace("@@TESTREFERENCES", testReferences);
+        fs.mkdirSync("./bin/tests/" + dir);
+        fs.writeFileSync("./bin/tests/" + dir + "/test.html", html);
+        tests += '    <h2><a href="' + dir + '/test.html">' + dir + " tests</a></h2>\r\n";
+    });
+    tests = tests.substr(0, tests.length - 2);
+    fs.writeFileSync("./bin/tests/tests.html", testMenuTemplate.replace("@@TESTS", tests));
 };
