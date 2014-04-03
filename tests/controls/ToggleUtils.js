443c197a135dd9e7537f342f4d5018804b423ee2
seanxu <seanxu@microsoft.com>
2014-04-03 16:30:28 -0700

    Changed back slashes to forward slashes in reference tags in all test files

--- tests/controls/ToggleUtils.js
+++ tests/controls/ToggleUtils.js
@@ -14,7 +14,7 @@
 /// <reference path="ms-appx://$(TargetFramework)/js/ui.js" />
 /// <reference path="ms-appx://$(TargetFramework)/js/en-us/ui.strings.js" />
 /// <reference path="ms-appx://$(TargetFramework)/css/ui-dark.css" />
-/// <reference path="..\TestLib\LegacyLiveUnit\commonutils.js"/>
+/// <reference path="../TestLib/LegacyLiveUnit/commonutils.js"/>
 /// <reference path="ToggleUtils.js"/>
 
 function ToggleUtils() {
