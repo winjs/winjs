abb8648849493d4821176d5e5f4622c428251f41
David Evans <david0evans@gmail.com>
2014-04-09 11:36:17 -0700

    Save attempt at phantomjs

---
+++ csstest/test.js
@@ -0,0 +1,30 @@
+﻿phantom.casperPath = "./CasperJs";
+phantom.injectJs(phantom.casperPath + "/bin/bootstrap.js");
+
+var casper = require("casper").create({
+    viewportSize: {
+        width: 800,
+        height: 600
+    }
+});
+
+var phantomcss = require("./PhantomCSS/phantomcss.js");
+var url = "http://www.google.com";
+
+phantomcss.init({
+    screenshotRoot: "./screenshots",
+    failedComparisonsRoot: "./failures"
+});
+
+casper.start(url)
+.then(function() {
+    phantomcss.screenshot("body", "test-page");
+})
+.then(function() {
+    phantomcss.compareAll();
+})
+.run(function() {
+    phantom.exit(phantomcss.getExitStatus());
+});
+
+phantom.exit();
