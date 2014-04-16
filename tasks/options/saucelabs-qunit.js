758fd89a2ae4c790e0ec39d56ca2b9099d54fea4
Edgar Banguero <edgar@live.com>
2014-04-16 16:58:07 -0700

    enabling navigation tests in saucelabs

--- tasks/options/saucelabs-qunit.js
+++ tasks/options/saucelabs-qunit.js
@@ -25,6 +25,7 @@
             options: {
                 urls: [
                     "http://127.0.0.1:9999/bin/tests/Promise/test.html?autostart",
+                    "http://127.0.0.1:9999/bin/tests/Navigation/test.html?autostart",
                     "http://127.0.0.1:9999/bin/tests/TimePicker/test.html?autostart",
                     "http://127.0.0.1:9999/bin/tests/WWA-Application/test.html?autostart",
                 ],
