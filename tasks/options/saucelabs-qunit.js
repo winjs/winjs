6e317e51b844a9317af3db119dcefabbbf987331
Edgar Banguero <edgar@live.com>
2014-04-23 16:58:30 -0700

    Update saucelabs-qunit.js

--- tasks/options/saucelabs-qunit.js
+++ tasks/options/saucelabs-qunit.js
@@ -24,11 +24,11 @@
         all: {
             options: {
                 urls: [
-                    "http://127.0.0.1:9999/bin/tests/Navigation/test.html?autostart",
-                    "http://127.0.0.1:9999/bin/tests/PageControl/test.html?autostart",
-                    "http://127.0.0.1:9999/bin/tests/Promise/test.html?autostart",
-                    "http://127.0.0.1:9999/bin/tests/TimePicker/test.html?autostart",
-                    "http://127.0.0.1:9999/bin/tests/WWA-Application/test.html?autostart",
+                    "http://127.0.0.1:9999/bin/tests/Navigation/test.html?autostart=true&disabletesttimeout=true",
+                    "http://127.0.0.1:9999/bin/tests/PageControl/test.html?autostart&disabletesttimeout=true",
+                    "http://127.0.0.1:9999/bin/tests/Promise/test.html?autostart&disabletesttimeout=true",
+                    "http://127.0.0.1:9999/bin/tests/TimePicker/test.html?autostart&disabletesttimeout=true",
+                    "http://127.0.0.1:9999/bin/tests/WWA-Application/test.html?autostart&disabletesttimeout=true",
                 ],
                 build: process.env.TRAVIS_JOB_ID,
                 testInterval: 1000,
