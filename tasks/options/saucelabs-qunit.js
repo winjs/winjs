7580422b435a07da640f863872ebaeae92a05c2a
Edgar Banguero <edgar@live.com>
2014-05-01 02:19:38 -0700

    change AppBarAndFlyouts testtimeout from 4 to 10 secs

--- tasks/options/saucelabs-qunit.js
+++ tasks/options/saucelabs-qunit.js
@@ -96,7 +96,7 @@
             options: {
                 urls: [
                     "http://127.0.0.1:9999/bin/tests/Animations/test.html?fastanimations=false&autostart=true",
-                    "http://127.0.0.1:9999/bin/tests/AppBarAndFlyouts/test.html?fastanimations=true&autostart=true&testtimeout=4000",
+                    "http://127.0.0.1:9999/bin/tests/AppBarAndFlyouts/test.html?fastanimations=true&autostart=true&testtimeout=10000",
                     "http://127.0.0.1:9999/bin/tests/Base/test.html?fastanimations=true&autostart=true",
                     "http://127.0.0.1:9999/bin/tests/Binding/test.html?fastanimations=true&autostart=true",
                     "http://127.0.0.1:9999/bin/tests/BindingList/test.html?fastanimations=true&autostart=true",
