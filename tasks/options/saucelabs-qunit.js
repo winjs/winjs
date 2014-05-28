2e6712736b1c9ca6c17d43d1be418ec5b22ef1c7
Edgar Banguero <edgar@live.com>
2014-05-27 19:55:11 -0700

    running ratings tests in ie11only since they timeout outside of IE

--- tasks/options/saucelabs-qunit.js
+++ tasks/options/saucelabs-qunit.js
@@ -119,7 +119,6 @@
                     "http://127.0.0.1:9999/bin/tests/Navigation/test.html?fastanimations=true&autostart=true",
                     "http://127.0.0.1:9999/bin/tests/PageControl/test.html?fastanimations=true&autostart=true",
                     "http://127.0.0.1:9999/bin/tests/Promise/test.html?fastanimations=true&autostart=true",
-                    "http://127.0.0.1:9999/bin/tests/Rating/test.html?fastanimations=true&autostart=true&testtimeout=3000",
                     "http://127.0.0.1:9999/bin/tests/Repeater/test.html?fastanimations=true&autostart=true",
                     "http://127.0.0.1:9999/bin/tests/Scheduler/test.html?fastanimations=true&autostart=true",
                     "http://127.0.0.1:9999/bin/tests/SearchBox/test.html?fastanimations=true&autostart=true",
@@ -146,7 +145,8 @@
                       "http://127.0.0.1:9999/bin/tests/ListView/test.html?fastanimations=true&autostart=true&testtimeout=10000",
                       "http://127.0.0.1:9999/bin/tests/ListViewIntegration/test.html?fastanimations=true&autostart=true&testtimeout=10000",
                       "http://127.0.0.1:9999/bin/tests/Pivot/test.html?fastanimations=false&autostart=true&testtimeout=10000",
-                      "http://127.0.0.1:9999/bin/tests/Tooltip/test.html?fastanimations=true&autostart=true&testtimeout=10000"
+                      "http://127.0.0.1:9999/bin/tests/Tooltip/test.html?fastanimations=true&autostart=true&testtimeout=10000",
+                      "http://127.0.0.1:9999/bin/tests/Rating/test.html?fastanimations=true&autostart=true&testtimeout=3000"
                 ],
                 build: process.env.TRAVIS_JOB_ID,
                 testInterval: 1000,
