3ffac8bac28d642bd40d979c1c023dc9d478bb53
Edgar Banguero <edgar@live.com>
2014-05-30 14:23:29 -0700

    Update saucelabs-qunit.js

--- tasks/options/saucelabs-qunit.js
+++ tasks/options/saucelabs-qunit.js
@@ -128,6 +128,7 @@
                     "http://127.0.0.1:9999/bin/tests/UI/test.html?fastanimations=true&autostart=true",
                     "http://127.0.0.1:9999/bin/tests/ViewBox/test.html?fastanimations=true&autostart=true",
                     "http://127.0.0.1:9999/bin/tests/WWA-Application/test.html?fastanimations=true&autostart=true", 
+                    "http://127.0.0.1:9999/bin/tests/Pivot/test.html?fastanimations=false&autostart=true&testtimeout=10000"
                 ],
                 build: process.env.TRAVIS_JOB_ID,
                 testInterval: 1000,
@@ -144,7 +145,6 @@
                 urls: [
                       "http://127.0.0.1:9999/bin/tests/ListView/test.html?fastanimations=true&autostart=true&testtimeout=10000",
                       "http://127.0.0.1:9999/bin/tests/ListViewIntegration/test.html?fastanimations=true&autostart=true&testtimeout=10000",
-                      "http://127.0.0.1:9999/bin/tests/Pivot/test.html?fastanimations=false&autostart=true&testtimeout=10000",
                       "http://127.0.0.1:9999/bin/tests/Tooltip/test.html?fastanimations=true&autostart=true&testtimeout=10000",
                       "http://127.0.0.1:9999/bin/tests/Rating/test.html?fastanimations=true&autostart=true&testtimeout=3000"
                 ],
