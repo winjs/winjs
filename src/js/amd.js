winjs/winjs

commit c2d6648c7e283e53370f611cc67867b6a90aa0f1

Enable "use strict"

src/js/amd.js
@@ -1,11 +1,11 @@
 // Copyright (c) Microsoft Open Technologies, Inc.  All Rights Reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+"use strict";
 /*jshint -W079*/
 var require;
 /*jshint +W079*/
 var define;
 
 (function() {
-    "use strict";
 
     var defined = {};
     /*jshint -W020*/

src/js/WinJS/Core/_Global.js
@@ -1,4 +1,5 @@
 // Copyright (c) Microsoft Open Technologies, Inc.  All Rights Reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 (function(global) {
+    "use strict";
     define(global);
 })(this);

src/js/base.js
@@ -17,6 +17,7 @@ define([
     'WinJS/ControlProcessor', 
     'WinJS/Controls/HtmlControl'
     ], function() {
+    "use strict";
 
 
     WinJS.Namespace.define("WinJS.Utilities", {

src/js/ui-phone.js
@@ -20,6 +20,7 @@ define([
     'WinJS/Controls/Tooltip',
     'WinJS/Controls/ViewBox'
     ], function() {
+    "use strict";
 
     return WinJS;
 });

src/js/ui.js
@@ -26,6 +26,7 @@ define([
     'WinJS/Controls/Tooltip',
     'WinJS/Controls/ViewBox'
     ], function() {
+    "use strict";
 
     //Wrapper module
 

tasks/options/connect.js
@@ -1,4 +1,5 @@
 // Copyright (c) Microsoft Open Technologies, Inc.  All Rights Reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+"use strict";
 var config = require("../../config.js");
 
 module.exports = {

tasks/utilities/.jshintrc
@@ -23,7 +23,7 @@
     "quotmark": false, // Enforces the consistency of quotation marks used throughout your code. 
     "undef": true, // Prohibits the use of explicitly undeclared variables.
     "unused": false, // Warns when you define and never use your variables.
-    "strict": false, // Requires all functions to run in ECMAScript 5's strict mode.
+    "strict": true, // Requires all functions to run in ECMAScript 5's strict mode.
     "trailing": false, // Makes it an error to leave a trailing whitespace in your code.
     "maxparams": false, // Lets you set the max number of formal parameters allowed per function.
     "maxdepth": false, // Lets you control how nested do you want your blocks to be.
@@ -54,7 +54,7 @@
     "moz": true, // Options tells JSHint that your code uses Mozilla JavaScript extensions. 
     "multistr": true, // Suppresses warnings about multi-line strings. Multi-line strings can be dangerous if you accidentally put a whitespace in between the escape character (\) and a new line.
     "notypeof": true, // Suppresses warnings about invalid typeof operator values. 
-    "proto": true, // Suppresses warnings about the __proto__ property.
+    "proto": false, // Suppresses warnings about the __proto__ property.
     "scripturl": true, // Suppresses warnings about the use of script-targeted URL's such as javascript:....
     "smarttabs": true, // Suppresses warnings about mixed tabs and spaces when the latter are used for alignmnent only.
     "shadow": true, // Suppresses warnings about variable shadowing i.e. declaring a variable that had been already declared somewhere in the outer scope.

tasks/utilities/require-style.js
@@ -2,6 +2,7 @@
 // Has special WinJS-specific behavior that wasn't achievable with libraries like require-less
 // Requires the 'platform' key to exist in the RequireJS config, with a value of 'desktop' or 'phone'
 define(['require'], function(req) {
+    "use strict";
 
     var api = {};
     var lessImports = [];

commit b4b8abf516670bd757b31df9f75125d6c9004930

Fix break caused by c2d6648c7e283e53370f611cc67867b6a90aa0f1

src/js/amd.js
@@ -1,11 +1,11 @@
 // Copyright (c) Microsoft Open Technologies, Inc.  All Rights Reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
-"use strict";
-/*jshint -W079*/
+/*jshint ignore:start */
 var require;
-/*jshint +W079*/
 var define;
+/*jshint ignore:end */
 
 (function() {
+    "use strict";
 
     var defined = {};
     define = function(id, dependencies, factory) {