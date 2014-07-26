d904476c25fa74909058c5a0447a1f087b154c96
Josh Williams <spankyj@gmail.com>
2014-07-26 10:23:55 -0700

    Restructured grunt requirejs task options to have defaults for most of them and only specify options where we need to override.
    Then created onefile task which can be used to build a single-file build for any single module with
    all of its dependencies (e.g. "grunt onefile:WinJS/Promise" or "grunt onefile:WinJS/Controls/AppBar").
    
    Used onefile task to build base.js and the single-file WinJS.js because those are canonical instances.

--- src/js/WinJS/Core/_Global.js
+++ src/js/WinJS/Core/_Global.js
@@ -1,5 +1,6 @@
 // Copyright (c) Microsoft Open Technologies, Inc.  All Rights Reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
-(function(global) {
+(function (global) {
     "use strict";
+
     define(global);
-})(this);
+}(this));
--- src/js/build/endWinJS-custom.js
+++
@@ -1,6 +0,0 @@
-// Copyright (c) Microsoft Open Technologies, Inc.  All Rights Reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
-        require('WinJS-custom', function (WinJS) {
-            global.WinJS = WinJS;
-        });
-    }));
-}(this));
