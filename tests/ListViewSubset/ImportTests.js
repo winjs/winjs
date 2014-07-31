b911cdaad42dcb6c1362eab256f40d9ad2eab09d
Sean Xu <jseanxu@live.com>
2014-07-31 14:36:07 -0700

    Rebalanced ListView tests into 3 separate folders for running tests in parallel.

--- tests/ListViewSubset/ImportTests.js
+++ tests/ListViewSubset/ImportTests.js
@@ -4,16 +4,12 @@
 /// <reference path="../ListView/AnimationTest.js" />
 /// <reference path="../ListView/AriaTest.js" />
 /// <reference path="../ListView/BackdropTest.js" />
-/// <reference path="../ListView/BrowseModeTest.js" />
-/// <reference path="../ListView/CommonLayout2Tests.js" />
 /// <reference path="../ListView/ConfigurationTest.js" />
 /// <reference path="../ListView/GroupFocusCache.js" />
 /// <reference path="../ListView/GroupListEditorTest.js" />
 /// <reference path="../ListView/GroupsTest.js" />
 /// <reference path="../ListView/HorizontalListTest.js" />
 /// <reference path="../ListView/InitializationTest.js" />
-/// <reference path="../ListView/Layout2Test.js" />
-/// <reference path="../ListView/LayoutTest.js" />
 /// <reference path="../ListView/ListEditorTest.js" />
 /// <reference path="../ListView/listView.js" />
 /// <reference path="../ListView/ListViewCancel.js" />
@@ -21,10 +17,15 @@
 /// <reference path="../ListView/MultisizeTest.js" />
 /// <reference path="../ListView/PropertyTest.js" />
 /// <reference path="../ListView/ReuseTest.js" />
-/// <reference path="../ListView/SelectionManagerTest.js" />
-/// <reference path="../ListView/SelectionModeTest.js" />
-/// <reference path="../ListView/VerticalGridTest.js" />
-/// <reference path="../ListView/VirtualizeContentsViewTest.js" />
+
+/// <reference path="../ListViewComponents/BrowseModeTest.js" />
+/// <reference path="../ListViewComponents/CommonLayout2Tests.js" />
+/// <reference path="../ListViewComponents/Layout2Test.js" />
+/// <reference path="../ListViewComponents/LayoutTest.js" />
+/// <reference path="../ListViewComponents/VirtualizeContentsViewTest.js" />
+/// <reference path="../ListViewComponents/VerticalGridTest.js" />
+/// <reference path="../ListViewComponents/SelectionManagerTest.js" />
+/// <reference path="../ListViewComponents/SelectionModeTest.js" />
 
 // FilterTests.js needs to be the first import since it hooks LiveUnit.registerClass.
  
