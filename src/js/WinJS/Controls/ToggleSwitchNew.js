eb3e32d40eaa254f15b4312a319e37473a386672
David Evans <david0evans@gmail.com>
2014-08-04 15:55:26 -0700

    Add xml comments, format properties and move their implementations out of private methods.

--- src/js/WinJS/Controls/ToggleSwitchNew.js
+++ src/js/WinJS/Controls/ToggleSwitchNew.js
@@ -14,6 +14,23 @@ define([
         "use strict";
 
         _Base.Namespace.define("WinJS.UI", {
+            /// <field>
+            /// <summary locid="WinJS.UI.ToggleSwitch">
+            /// A control that lets the user switch an option on or off.
+            /// </summary>
+            /// </field>
+            /// <icon src="ui_winjs.ui.toggleswitch.12x12.png" width="12" height="12" />
+            /// <icon src="ui_winjs.ui.toggleswitch.16x16.png" width="16" height="16" />
+            /// <htmlSnippet><![CDATA[<div data-win-control="WinJS.UI.ToggleSwitch"></div>]]></htmlSnippet>
+            /// <event name="change" bubbles="true" locid="WinJS.UI.ToggleSwitch_e:change">Raised when the switch is flipped to on (checked is set to true) or off (checked is set to false). </event>
+            /// <part name="toggle" class="win-toggleSwitch" locid="WinJS.UI.ToggleSwitch_part:toggle">The entire ToggleSwitch control.</part>
+            /// <part name="switch" class="win-switch" locid="WinJS.UI.ToggleSwitch_part:switch">The slider that enables the user to switch the state of the ToggleSwitch.</part>
+            /// <part name="title" class="win-title" locid="WinJS.UI.ToggleSwitch_part:title">The main text for the ToggleSwitch control.</part>
+            /// <part name="label-on" class="win-on" locid="WinJS.UI.ToggleSwitch_part:label-on">The text for when the switch is on.</part>
+            /// <part name="label-off" class="win-off" locid="WinJS.UI.ToggleSwitch_part:label-off:">The text for when the switch is off.</part>
+            /// <resource type="javascript" src="//$(TARGET_DESTINATION)/js/base.js" shared="true" />
+            /// <resource type="javascript" src="//$(TARGET_DESTINATION)/js/ui.js" shared="true" />
+            /// <resource type="css" src="//$(TARGET_DESTINATION)/css/ui-dark.css" shared="true" />
             ToggleSwitchNew: _Base.Namespace._lazy(function() {
 
                 // Store some class names
@@ -33,8 +50,37 @@ define([
                 var classDragging = 'win-toggleswitch-dragging-new';
                 var classPressed = 'win-toggleswitch-pressed-new';
 
+                // Localized strings
+                var strings = {
+                    get on() { 
+                        return _Resources._getWinJSString("ui/on").value; 
+                    },
+                    get off() { 
+                        return _Resources._getWinJSString("ui/off").value; 
+                    },
+                };
+
                 // Define the ToggleSwitch class
                 var Toggle = _Base.Class.define(function ToggleSwitchNew_ctor(element, options) {
+                    /// <signature helpKeyword="WinJS.UI.ToggleSwitch.ToggleSwitch">
+                    /// <summary locid="WinJS.UI.ToggleSwitch.constructor">
+                    /// Creates a new ToggleSwitch.
+                    /// </summary>
+                    /// <param name="element" domElement="true" locid="WinJS.UI.ToggleSwitch.constructor_p:element">
+                    /// The DOM element that hosts the ToggleSwitch.
+                    /// </param>
+                    /// <param name="options" type="Object" locid="WinJS.UI.ToggleSwitch.constructor_p:options">
+                    /// An object that contains one or more property/value pairs to apply to the new control.
+                    /// Each property of the options object corresponds to one of the control's properties or events.
+                    /// Event names must begin with "on". For example, to provide a handler for the change event,
+                    /// add a property named "onchange" to the options object and set its value to the event handler.
+                    /// This parameter is optional.
+                    /// </param>
+                    /// <returns type="WinJS.UI.ToggleSwitch" locid="WinJS.UI.ToggleSwitch.constructor_returnValue">
+                    /// The new ToggleSwitch.
+                    /// </returns>
+                    /// </signature>
+
                     // Main container
                     element = element || _Global.document.createElement('div');
                     this._domElement = element;
@@ -75,10 +121,10 @@ define([
                     this._domElement.addEventListener('keydown', this._keyDownHandler.bind(this));
                     this._clickElement.addEventListener('mousedown', this._pointerDownHandler.bind(this));
                     this._clickElement.addEventListener('touchstart', this._pointerDownHandler.bind(this));
-                    window.addEventListener('mousemove', this._pointerMoveHandler.bind(this));
-                    window.addEventListener('touchmove', this._pointerMoveHandler.bind(this));
-                    window.addEventListener('mouseup', this._pointerUpHandler.bind(this));
-                    window.addEventListener('touchend', this._pointerUpHandler.bind(this));
+                    _ElementUtilities._globalListener.addEventListener('mousemove', this._pointerMoveHandler.bind(this));
+                    _ElementUtilities._globalListener.addEventListener('touchmove', this._pointerMoveHandler.bind(this));
+                    _ElementUtilities._globalListener.addEventListener('mouseup', this._pointerUpHandler.bind(this));
+                    _ElementUtilities._globalListener.addEventListener('touchend', this._pointerUpHandler.bind(this));
 
                     // Current x coord while being dragged
                     this._dragX = 0;
@@ -86,38 +132,115 @@ define([
                     // Default state
                     this.checked = false;
                     this.disabled = false;
-                    this.labelOn = 'On';
-                    this.labelOff = 'Off';
+                    this.labelOn = strings.on;
+                    this.labelOff = strings.off;
 
                     // Apply options
                     _Control.setOptions(this, options);
                 }, {
                     // Properties
+
+                    /// <field type='HTMLElement' domElement='true' hidden='true' locid="WinJS.UI.ToggleSwitch.element" helpKeyword="WinJS.UI.ToggleSwitch.element">
+                    /// The DOM element that hosts the ToggleSwitch control.
+                    /// </field>
                     element: {
-                        get: function() {return this._domElement;}
+                        get: function() {
+                            return this._domElement;
+                        }
                     },
+                    /// <field type="Boolean" locid="WinJS.UI.ToggleSwitch.checked" helpKeyword="WinJS.UI.ToggleSwitch.checked">
+                    /// Gets or sets whether the control is on (checked is set to true) or off (checked is set to false).
+                    /// </field>
                     checked: {
-                        get: function() {return this._checked;},
-                        set: function(value) {this._setChecked(value);}
+                        get: function() {
+                            return this._checked;
+                        },
+                        set: function(value) {
+                            if (this.disabled) {
+                                return;
+                            }
+
+                            value = !!value;
+                            if (value === this.checked) {
+                                return;
+                            }
+
+                            this._checked = value;
+                            if (value) {
+                                this._labelOnElement.style.display = '';
+                                this._labelOffElement.style.display = 'none';
+                                _ElementUtilities.addClass(this._domElement, classOn);
+                                _ElementUtilities.removeClass(this._domElement, classOff);
+                            } else {
+                                this._labelOnElement.style.display = 'none';
+                                this._labelOffElement.style.display = '';
+                                _ElementUtilities.addClass(this._domElement, classOff);
+                                _ElementUtilities.removeClass(this._domElement, classOn);
+                            }
+                        }
                     },
+                    /// <field type="Boolean" locid="WinJS.UI.ToggleSwitch.disabled" helpKeyword="WinJS.UI.ToggleSwitch.disabled">
+                    /// Gets or sets a value that specifies whether the control is disabled.
+                    /// </field>
                     disabled: {
-                        get: function() {return this._disabled;},
-                        set: function(value) {this._setDisabled(value);}
+                        get: function() {
+                            return this._disabled;
+                        },
+                        set: function(value) {
+                            value = !!value;
+                            if (value === this._disabled) {
+                                return;
+                            }
+
+                            if (value) {
+                                _ElementUtilities.addClass(this._domElement, classDisabled);
+                            } else {
+                                _ElementUtilities.removeClass(this._domElement, classDisabled);
+                            }
+
+                            this._disabled = value;
+                        }
                     },
+                    /// <field type="String" locid="WinJS.UI.ToggleSwitch.labelOn" helpKeyword="WinJS.UI.ToggleSwitch.labelOn">
+                    /// Gets or sets the text that displays when the control is on (checked is set to true). The default value is "On".
+                    /// </field>
                     labelOn: {
-                        get: function() {return this._labelOnElement.innerHTML;},
-                        set: function(value) {this._labelOnElement.innerHTML = value;}
+                        get: function() {
+                            return this._labelOnElement.innerHTML;
+                        },
+                        set: function(value) {
+                            this._labelOnElement.innerHTML = value;
+                        }
                     },
+                    /// <field type="String" locid="WinJS.UI.ToggleSwitch.labelOff" helpKeyword="WinJS.UI.ToggleSwitch.labelOff">
+                    /// Gets or sets the text that displays when the control is off (checked is set to false). The default value is "Off".
+                    /// </field>
                     labelOff: {
-                        get: function() {return this._labelOffElement.innerHTML;},
-                        set: function(value) {this._labelOffElement.innerHTML = value;}
+                        get: function() {
+                            return this._labelOffElement.innerHTML;
+                        },
+                        set: function(value) {
+                            this._labelOffElement.innerHTML = value;
+                        }
                     },
+                    /// <field type='String' locid="WinJS.UI.ToggleSwitch.title" helpKeyword="WinJS.UI.ToggleSwitch.title">
+                    /// Gets or sets the main text for the ToggleSwitch control. This text is always displayed, regardless of whether
+                    /// the control is switched on or off.
+                    /// </field>
                     title: {
-                        get: function() {return this._headerElement.innerHTML;},
-                        set: function(value) {this._headerElement.innerHTML = value;}
+                        get: function() {
+                            return this._headerElement.innerHTML;
+                        },
+                        set: function(value) {
+                            this._headerElement.innerHTML = value;
+                        }
                     },
 
                     // Events
+
+                    /// <field type="Function" locid="WinJS.UI.ToggleSwitch.onchange" helpKeyword="WinJS.UI.ToggleSwitch.onchange">
+                    /// Occurs when the ToggleSwitch control is flipped to on (checked == true) or off (checked == false).
+                    /// </field>
                     onchange: _Events._createEventProperty('change'),
 
                     // Public methods
@@ -129,46 +252,7 @@ define([
                         this._disposed = true;
                     },
 
-                    // Private methods
-                    _setChecked: function ToggleSwitchNew_setChecked(value) {
-                        if (this.disabled) {
-                            return;
-                        }
-
-                        value = !!value;
-                        if (value === this.checked) {
-                            return;
-                        }
-
-                        this._checked = value;
-                        if (value) {
-                            this._labelOnElement.style.display = '';
-                            this._labelOffElement.style.display = 'none';
-                            _ElementUtilities.addClass(this._domElement, classOn);
-                            _ElementUtilities.removeClass(this._domElement, classOff);
-                        } else {
-                            this._labelOnElement.style.display = 'none';
-                            this._labelOffElement.style.display = '';
-                            _ElementUtilities.addClass(this._domElement, classOff);
-                            _ElementUtilities.removeClass(this._domElement, classOn);
-                        }
-                    },
-                    _setDisabled: function ToggleSwitchNew_setDisabled(value) {
-                        value = !!value;
-                        if (value === this._disabled) {
-                            return;
-                        }
-
-                        if (value) {
-                            _ElementUtilities.addClass(this._domElement, classDisabled);
-                        } else {
-                            _ElementUtilities.removeClass(this._domElement, classDisabled);
-                        }
-
-                        this._disabled = value;
-                    },
-
-                    // Event handlers
+                    // Private event handlers
                     _keyDownHandler: function(e) {
                         e.preventDefault();
                         
