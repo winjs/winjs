a3576cfeaee505f02c7fb8c2236a4052037d3a76
Jeff Fisher <jeffish@microsoft.com>
2014-08-06 11:27:45 -0700

    Fix reporter.

--- tasks/utilities/jscsreporter.js
+++ tasks/utilities/jscsreporter.js
@@ -1,14 +1,19 @@
-var util = require('util');
+// Copyright (c) Microsoft Open Technologies, Inc.  All Rights Reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+(function () {
+    "use strict";
+
+    var util = require('util');
 
 
-module.exports = function (errorsCollection) {
-    errorsCollection.forEach(function (errors) {
-        if (!errors.isEmpty()) {
-            var file = errors.getFilename();
-            var out = errors.getErrorList().map(function (error) {
-                return util.format('%s: line %d, col %d, %s', file, error.line, error.column + 1, error.message);
-            });
-            console.log(out.join('\n'));
-        }
-    });
-};
+    module.exports = function (errorsCollection) {
+        errorsCollection.forEach(function (errors) {
+            if (!errors.isEmpty()) {
+                var filename = errors.getFilename();
+                var output = errors.getErrorList().map(function (error) {
+                    return util.format('%s: line %d, col %d, %s', filename, error.line, error.column + 1, error.message);
+                });
+                console.log(output.join('\n'));
+            }
+        });
+    };
+})();
