4354a3c8e2a68f7a4e9ed333ade98af23c74588a
Edgar Banguero <edgar@live.com>
2014-09-25 09:40:40 -0700

    Execute _copyToTsBuild prior to onefile task

--- tasks/onefile.js
+++ tasks/onefile.js
@@ -2,9 +2,19 @@
 (function () {
     "use strict";
 
-    module.exports = function (grunt) {
+    var copiedToTsBuild = false;
 
-        grunt.registerTask("onefile", "build a single module into a full WinJS build", function (path) {
+    module.exports = function (grunt) {
+        grunt.registerTask("onefile", "build a single module into a full WinJS build", function(path) {
+            if (copiedToTsBuild) {
+                grunt.task.run(["_buildModule:" + path]);
+            } else {
+                grunt.task.run(["_copyToTsBuild", "_buildModule:" + path]);
+                copiedToTsBuild = true;
+            }
+        });
+
+        grunt.registerTask("_buildModule", "", function (path) {
             var requirejs = grunt.config.get("requirejs");
             var merge = { requirejs: {} };
             var config = requirejs.defaults(path);
