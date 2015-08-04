6391a6330eca6240802ba2c1eabe5a2f809e6862
jessesh <jessesh@microsoft.com>
2015-08-03 23:44:31 -0700

    no message

--- tools/d.ts-verifier/main.js
+++ tools/d.ts-verifier/main.js
@@ -1,5 +1,4 @@
 // Copyright (c) Microsoft Corporation.  All Rights Reserved. Licensed under the MIT License. See License.txt in the project root for license information.
-#!/usr/bin/env node
 
 "use strict";
 
