e94e4ac7b4a8a672bb58beec1ee78aa0bcc4b2aa
jessesh <jessesh@microsoft.com>
2015-09-15 16:20:13 -0700

    no message

--- tests/ElementResize/elementResizeInstrumentTests.ts
+++ tests/ElementResize/elementResizeInstrumentTests.ts
@@ -531,6 +531,67 @@ module CorsicaTests {
 
         }
 
+        testReAppendToDomAndResizeAsynchronouslyExtended(complete) {
+            // Make sure that removing and reappending an initialized _ElementResizeInstrument
+            // Doesn't permanently stop our _ElementResizeInstrument from firing resize events.
+            // This test is partially testing the browser to make sure that the "resize" listener 
+            // we've added to the <object> element's contentWindow doesn't become permanently 
+            // broken it it leaves and renters the DOM.
+            var childResizeSignal: WinJS._Signal<any>;
+            function childResizeHandler() {
+                childResizeSignal.complete();
+            }
+
+            var parentResizeSignal: WinJS._Signal<any>;
+            function parentResizeHandler() {
+                parentResizeSignal.complete();
+            }
+
+            var parent = this._parent;
+            var parentInstrument = this._parentInstrument;
+            var childInstrument = this._childInstrument;
+
+            parentInstrument.addedToDom();
+            childInstrument.addedToDom();
+
+            WinJS.Promise
+                .join([
+                    awaitInitialResizeEvent(this._parentInstrument),
+                    awaitInitialResizeEvent(this._childInstrument)
+                ])
+                .then(() => {
+                    parentInstrument.addEventListener(resizeEvent, parentResizeHandler);
+                    childInstrument.addEventListener(resizeEvent, childResizeHandler);
+
+                    // Test both instruments still fire resize after asynchronously
+                    // removing, adding and updating the width of the parent element.
+                    parentResizeSignal = new WinJS._Signal();
+                    childResizeSignal = new WinJS._Signal();
+
+                    this._element.removeChild(parent);
+                    return new WinJS.Promise((c) => {
+                        window.requestAnimationFrame(c);
+                    });
+                })
+                .then(() => {
+                    this._element.appendChild(parent);
+                    return new WinJS.Promise((c) => {
+                        window.requestAnimationFrame(c);
+                    });
+                })
+                .then(() => {
+                    parent.style.width = "43%"
+                    return WinJS.Promise.join([
+                        parentResizeSignal.promise,
+                        childResizeSignal.promise,
+                    ]);
+                })
+                .done(() => {
+                    complete();
+                })
+
+        }
+
         //testObjectScenario1(complete) {
         //    var scenario = 1;
         //    var objEl = getnewObj();
