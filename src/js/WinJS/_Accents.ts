bb646068db75e01c477c1360d392aa82ca61951d
Sean Xu <jseanxu@live.com>
2015-11-20 13:15:14 -0800

    Added design documentations to Pivot, XYFocus, ASB and Accent color features.

--- src/js/WinJS/_Accents.ts
+++ src/js/WinJS/_Accents.ts
@@ -1,4 +1,66 @@
 // Copyright (c) Microsoft Corporation.  All Rights Reserved. Licensed under the MIT License. See License.txt in the project root for license information.
+
+//High Level
+//  - Injects and maintains a dynamic stylesheet to style elements for user
+//    accent colors.The RGB values of the colors are not available until run
+//    time and may also change at any time during run time.
+//	- Allows CSS syntax styling of elements to apply user accent colors via
+//    code, e.g.:
+//      Accents.createAccentRule(".myControl input", [
+//          { name: "color", value: Accents.ColorTypes.accent },
+//          { name: "border-color", value: Accents.ColorTypes.accent }
+//      ]);
+//      Note: The Accents Module is currently only available internally to
+//            WinJS controls.
+//	- Supports
+//      - Primary Accent color
+//      - Redline specific accent shades
+//  - Picks the right shading depending on ui- light or ui- dark
+
+//Theme detection
+// Since accent color shades vary depending on the theme of the app
+// (light vs.dark), we first need to detect which theme is currently applied.
+// This detection is done once, during app launch, and the scenario where the
+// theme stylesheet is swapped at runtime is not supported.
+// The ui- dark stylesheet contains the following rule that ui- light does not:
+//      winjs - themedetection - tag {
+//          opacity: 0;
+//      }
+// The accent color implementation injects an element <winjs-themedetection-tag />
+// into the < head > at launch and examines if the computed opacity is 0 to see if
+// we are in the dark theme.This tag is removed after examination.
+
+//Specificity Concerns
+// WinJS apps headers look like this:
+//  <head>
+//      <link href="ui-dark.css" rel= "stylesheet" />
+//      <script src="base.js"></script>
+//      <script src="ui.js"></script>
+//      <link href="myApp.css" rel="stylesheet" />
+//      <script src="myApp.js"></script>
+//  </head>
+
+//The dynamic stylesheet is always on the bottom of the head section of the
+// document, which means that it will trump WinJS and app developer styles that
+// have the same specificity.This is intended as we consider accent colors as
+// part of a control's feature implementation. If the app developer wants to
+// forcefully overwrite the accent color of a specific element, they are expected
+// to write a more specific selector.
+
+//Availability
+// Real accent colors are only available when WinRT is present.We depend on the
+// _WinRT.Windows.UI.ViewManagement.UISettings.colorValuesChanged event to
+// set/update the accent colors.
+// When WinRT is unavailable, we use a hard- coded value as the accent color as
+// a fallback.WinJS doesn’t currently support a way to change this fallback value.
+
+//Implementation
+// The Accent Color module injects a dynamic stylesheet into the head of the
+// document and updates that style when new accent rules are added.It batches
+// updates so that multiple, synchronous additions only trigger one stylesheet
+// rewrite.The full implementation consists of many, many string concatenations
+// which are much better documented in the actual source code.
+
 import _Global = require("./Core/_Global");
 import _WinRT = require("./Core/_WinRT");
 
